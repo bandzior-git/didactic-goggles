bddc0ec9f072cf7294f2aff4b9f39b9d5ff514d1
bandzior-git <balcer77@gmail.com>
2023-04-05 15:35:09 +0200

    Added today date to see changes

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React on 2023.04.05
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
